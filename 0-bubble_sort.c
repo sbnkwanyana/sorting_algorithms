c168858a56f1273c56a42681274ecc2cf122324f
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-10-11 11:04:04 +0200

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,36 @@
 #include "sort.h"
 
 /**
- * bubble_sort - 
- * @array: 
- * @size:
+ * bubble_sort - implementation of the bubble sort algorithm
+ * thats sorts an array of integers
+ * @array: unsorted array to sort
+ * @size: iterations to pass through the array
  * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i;
+	int swap;
+	int tmp;
 
+	if (size == 0 || array == NULL)
+		return;
+
+	swap = 1;
+	while (swap)
+	{
+		swap = 0;
+
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -1,3 +1,7 @@
+#ifndef SORT_HEADER
+
+#define SORT_HEADER
+
 #include <stdlib.h>
 
 /**
@@ -9,12 +13,14 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+#endif
