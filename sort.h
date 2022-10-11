b04c8973d8c1e0c46742c0190863a5eb270fe903
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-10-11 11:04:56 +0200

    header file update

--- sort.h
+++ sort.h
@@ -22,5 +22,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void print_array(int *array, size_t size);
+void print_list(listint_t *list);
 
 #endif
